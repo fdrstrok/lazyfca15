c261cf7cd7772dcf40c9476f90c580ceb1e5b50b
PolinaIv <ivanova.p.m@gmail.com>
2015-12-24 00:07:05 +0300

    Final. Fixed algorithms and report

--- ivanova_polina/cross_validation.py
+++ ivanova_polina/cross_validation.py
@@ -12,39 +12,45 @@ plus = [a for a in train if a[-1] == "positive"]
 minus = [a for a in train if a[-1] == "negative"]
 q.close()
 
+train = train[1:]
+#train = train[530:580]
+
 def cv_f(threshold, check_function):
-    global train, cv_res
+    global train
     nf = 5
     kf = cross_validation.KFold(len(train), n_folds=nf, shuffle=True, random_state=0)
 
     accuracy = 0
     i = 0
     for train_index, test_index in kf:
-        if i>0:
-            return accuracy, 2
+        if i>2:
+            return accuracy/3
         print(i)
         i += 1
         X_train, X_test = list( train[i] for i in train_index ), list( train[i] for i in test_index )
         plus = [a for a in X_train if a[-1] == "positive"]
         minus = [a for a in X_train if a[-1] == "negative"]
         cv_res = { "positive_positive": 0, "positive_negative": 0, "negative_positive": 0, "negative_negative": 0, "contradictory": 0}
+        print(test_index)
         for elem in X_test:
-            check_function(plus, minus, elem, threshold)
+            cv_res_temp = check_function(plus, minus, elem, threshold, cv_res)
+            if cv_res_temp is not None:
+                cv_res = cv_res_temp
+            print(cv_res)
         cvv = [s for s in cv_res.values()]
         print( sum(cvv) )
-        print(cv_res)
         print((cv_res["positive_positive"] + cv_res["negative_negative"]) / len(X_test))
         accuracy += (cv_res["positive_positive"] + cv_res["negative_negative"]) / len(X_test) #/ sum(cvv)
     accuracy /= nf
     return accuracy
 
 # For algorithm_1.py
-threshold = 0.127
-accuracy = cv_f(threshold, check_hypothesis_1)
-print(accuracy)
-
-# # For algorithm_2.py
-# threshold = 0.125
-# accuracy = cv_f(threshold, check_hypothesis_2)
+# threshold = 0.127
+# accuracy = cv_f(threshold, check_hypothesis_1)
 # print(accuracy)
+
+# For algorithm_2.py
+threshold = 0.6
+accuracy = cv_f(threshold, check_hypothesis_2)
+print(accuracy)
 
